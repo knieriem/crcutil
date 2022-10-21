dc6b23f740fe769640fba5f89a3e8c0f5e43ba1c
Michael Teichgräber <teichgraeber@t9rsys.com>
2022-10-22 00:59:21 +0200

    Poly.MakeTable: replace arguments by functional table options
    Also, convert ReverseTableBits to WithReversedBits table option.

--- crcutil.go
+++ crcutil.go
@@ -12,27 +12,59 @@ type Word interface {
 // MakeTable creates a lookup table for the specified polynomial.
 // The size of the table returned equals the length of
 // the value range determined by the number of data bits.
-func (poly *Poly[T]) MakeTable(initial T, dataWidth int) []T {
-	N := 1 << dataWidth
+func (poly *Poly[T]) MakeTable(opts ...TableOption) []T {
+	var conf tableConf
+	conf.dataWidth = 8
+	for _, o := range opts {
+		o(&conf)
+	}
+	N := 1 << conf.dataWidth
 
 	updateBitwise := BitwiseUpdateFn[T, T](poly)
 
 	tab := make([]T, N)
 	for i := range tab {
-		tab[i] = updateBitwise(poly, initial, T(i), dataWidth)
+		tab[i] = updateBitwise(poly, T(conf.initial), T(i), conf.dataWidth)
+		if conf.reverseBits {
+			tab[i] = reverseBits(tab[i], poly.Width)
+		}
 	}
 	return tab
 }
 
-// ReverseTableBits mirrors the lower bits of each table entry within
-// the bit range specified by width.
-// The calculations are done in-place, so the original table
-// is altered; the modified table is also returned for convenience.
-func ReverseTableBits[T Word](tab []T, width int) []T {
-	for i := range tab {
-		tab[i] = reverseBits(tab[i], width)
+type TableOption func(*tableConf)
+
+type tableConf struct {
+	initial     uint32
+	dataWidth   int
+	reverseBits bool
+}
+
+// WithDataWidthByte sets the data width to 8 bits,
+// which will result in a table of 256 entries for byte-wise processing.
+func WithDataWidth(w int) TableOption {
+	return func(c *tableConf) {
+		c.dataWidth = w
 	}
-	return tab
+}
+
+// WithInitialValue ensures that when a table is created
+// the specified initial value will be applied to the
+// calculation of each table entry.
+// In cases where a table later is used manually,
+// like when a CRC is calulated over some bits only,
+// this saves one XOR operation.
+func WithInitialValue(initial uint32) TableOption {
+	return func(c *tableConf) {
+		c.initial = initial
+	}
+}
+
+// WithReversedBits table option mirrors the bits of each table entry.
+func WithReversedBits() TableOption {
+	return func(c *tableConf) {
+		c.reverseBits = true
+	}
 }
 
 // BitwiseUpdateFn returns a function that returns the result of
